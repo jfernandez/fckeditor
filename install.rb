6a7c808cf959818f2f6f8ce382155c33818b2620
srutherford <srutherford@13c73f65-831a-0410-84ff-c4c9c9a2e73e>
2006-09-11 11:47:37 +0000

    bug fix in install for versioning
    git-svn-id: svn://rubyforge.org//var/svn/fckeditorp/trunk/fckeditor@28 13c73f65-831a-0410-84ff-c4c9c9a2e73e

--- install.rb
+++ install.rb
@@ -2,10 +2,11 @@
 
 directory = File.join(RAILS_ROOT, '/vendor/plugins/fckeditor/')
 require "#{directory}lib/fckeditor_file_utils"
+require "#{directory}lib/fckeditor_version"
 require "#{directory}lib/fckeditor"
 
-puts "** Installing FCKEditor Plugin version #{Fckeditor.version}...." 
+puts "** Installing FCKEditor Plugin version #{FckeditorVersion.current}...." 
 
 FckeditorFileUtils.destroy_and_install
 
-puts "** Successfully installed FCKEditor Plugin version #{Fckeditor.version}"
+puts "** Successfully installed FCKEditor Plugin version #{FckeditorVersion.current}"
