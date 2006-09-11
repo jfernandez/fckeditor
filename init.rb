af4d761111a743736084d5f84252b9e0fb505467
srutherford <srutherford@13c73f65-831a-0410-84ff-c4c9c9a2e73e>
2006-09-11 21:25:09 +0000

    fixed test issue
    git-svn-id: svn://rubyforge.org//var/svn/fckeditorp/trunk/fckeditor@29 13c73f65-831a-0410-84ff-c4c9c9a2e73e

--- init.rb
+++ init.rb
@@ -15,16 +15,35 @@ ActionView::Base.send(:include, Fckeditor::Helper)
 # require the controller
 require 'fckeditor_controller'
 
+# # add a route for spellcheck
+# class ActionController::Routing::RouteSet
+#   alias draw_without_spelling draw
+#   def draw_with_spelling
+#     draw_without_spelling do |map|
+#       map.connect 'fckeditor/check_spelling', :controller => 'fckeditor', :action => 'check_spelling'
+#       map.connect 'fckeditor/command', :controller => 'fckeditor', :action => 'command'
+#       map.connect 'fckeditor/upload', :controller => 'fckeditor', :action => 'upload'
+#       yield map
+#     end
+#   end
+#   alias draw draw_with_spelling
+# end
+
 # add a route for spellcheck
 class ActionController::Routing::RouteSet
-  alias draw_without_spelling draw
-  def draw_with_spelling
-    draw_without_spelling do |map|
-      map.connect 'fckeditor/check_spelling', :controller => 'fckeditor', :action => 'check_spelling'
-      map.connect 'fckeditor/command', :controller => 'fckeditor', :action => 'command'
-      map.connect 'fckeditor/upload', :controller => 'fckeditor', :action => 'upload'
-      yield map
-    end
+  unless (RAILS_ENV.match('test'))
+    class_eval <<-"end_eval", __FILE__, __LINE__  
+      alias draw_without_fckeditor draw
+      def draw_with_fckeditor
+        draw_without_fckeditor do |map|
+          map.connect 'fckeditor/check_spelling', :controller => 'fckeditor', :action => 'check_spelling'
+          map.connect 'fckeditor/command', :controller => 'fckeditor', :action => 'command'
+          map.connect 'fckeditor/upload', :controller => 'fckeditor', :action => 'upload'
+          yield map
+        end
+      end
+      alias draw draw_with_fckeditor
+    end_eval
   end
-  alias draw draw_with_spelling
 end
+
